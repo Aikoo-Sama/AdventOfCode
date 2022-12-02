aab096276e7ecf2f5606a1428cbe0c697681c835
Aiko <46852063+ItsJustAiko@users.noreply.github.com>
2022-12-02 16:06:49 +0100

    Finish day 01 in python

--- 2022/Python/01/main.py
+++ 2022/Python/01/main.py
@@ -1,27 +1,27 @@
-
-
 with open("list.txt", 'r') as file:
     data = file.read().split('\n')
 
-    biggest = 0
-    second = 0
-    last = 0
+    sumOfAllElves = []
     add = 0
 
     for element in data:
         if (element == ''):
-            print("Add ", add)
-            if (add >= biggest):
-                biggest = add
-            if (add >= second and add < biggest):
-                second = add
-            if (add >= last or last == 0):
-                last = add
-
+            sumOfAllElves.append(add)
             add = 0
         else:
             add += int(element)
 
+    biggest = max(sumOfAllElves)
+    second = 0
+    last = 0
+
+    for element in sumOfAllElves:
+        if (element > second and element < biggest):
+            second = element
+        if (element > last and element < second):
+            last = element
+
     print("Biggest: ", biggest)
     print("Second: ", second)
     print("Last: ", last)
+    print(biggest + second + last)
