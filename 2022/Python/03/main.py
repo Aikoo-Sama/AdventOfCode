5753aa6d12eeea37d75ab361aa5e57b96cc02888
Aiko <46852063+ItsJustAiko@users.noreply.github.com>
2022-12-06 16:42:39 +0100

    Day 03 part 1

--- 2022/Python/03/main.py
+++ 2022/Python/03/main.py
@@ -1,26 +1,26 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 with open("backpack.txt", 'r') as file:
     backpacks = file.read().split("\n")
-    letters = {}
     total = 0
+    arr = []
 
     for backpack in backpacks:
         if (backpack == ""):
             continue
-        backpack = list(backpack)
+
+
         first = backpack[:len(backpack) // 2]
         second = backpack[len(backpack) // 2:]
 
         for letter in first:
-            if letter in second:
-                if letter in letters:
-                    letters[letter] += 1
-                else:
-                    letters[letter] = 1
+            for letter2 in second:
+                if (letter == letter2):
+                    arr.append(letter)
+                    second = second.replace(letter, "")
+                    break
 
-
-for letter in letters:
-    total += alphabet.index(letter) + 1
+for letter in arr:
+        total += alphabet.index(letter.lower()) + 1 if letter.islower() else alphabet.index(letter.lower()) + 27
 
 print(total)
