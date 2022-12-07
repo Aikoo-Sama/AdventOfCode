d2bcc3dd402dbcfc2b7f9dd84a885bbc0448ae69
Aiko <46852063+ItsJustAiko@users.noreply.github.com>
2022-12-07 15:32:41 +0100

    Optimize code

--- 2022/Python/03/partOne/main.py
+++ 2022/Python/03/partOne/main.py
@@ -14,11 +14,8 @@ with open("backpack.txt", 'r') as file:
         second = backpack[len(backpack) // 2:]
 
         for letter in first:
-            for letter2 in second:
-                if (letter == letter2):
-                    arr.append(letter)
-                    second = second.replace(letter, "")
-                    break
+            arr.append(letter) if letter in second else None
+            second = second.replace(letter, "") if letter in second else second
 
 for letter in arr:
         total += alphabet.index(letter.lower()) + 1 if letter.islower() else alphabet.index(letter.lower()) + 27
